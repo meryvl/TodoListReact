e7c811dce5858ab43030243b8711919b5e837094
meryvl <meryvalerolaguna@gmail.com>
2023-03-13 12:16:56 +0000

    new commit

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -4,29 +4,65 @@ const TodoList =()=>{
     const [elements, setElements] = useState([]);                 
     const [inputValue, setInputValue] = useState("");       
 
-const addElement = (todo) => {
-        if(inputValue !== ""){
-            const newList = {
-                id: Math.random(),
-                todo: todo
-            }
-            setElements([...elements, newList]);
-        }   
-            setInputValue("");    
-    };
+    const getElement =()=>{
+    return(
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/meryvl')
+		.then((res) => res.json())
+		.then((res) =>{
+			console.log("Perfect!!",res)
+            setElements(res)
+		})
+		.catch(eror =>console.log(eror))
+	)
+}
+useEffect(getElement,[])
 
-const handleDeleteElement =(id)=>{
-const newListFilter = elements.filter((todo) => todo.id !== id);
-setElements(newListFilter);
+const putElement=(newList)=>{
+    console.log(newList)
+    return(
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/meryvl',{
+            method:'PUT',
+            body:JSON.stringify(newList),
+            headers:{
+                "Content-Type": "application/json",
+            }
+        })
+		.then(() =>{
+			getElement()
+            
+		})
+		.catch(eror =>console.log(eror))
+    )}
+
+
+
+    const update = () => {
+        if(inputValue !== ""){
+          const newElement = {
+                label: inputValue,
+                done: false
+          }
+            setInputValue("");
+            putElement([...elements, newElement])
+          
+       
+        }  
+           
+            
+    };
+    
+
+const handleDeleteElement =(label)=>{
+const newListFilter = elements.filter((todo) => todo.label !== label);
+putElement(newListFilter);
 }
     
 const onKeyPress = (event) => {
     if (event.key === "Enter") {
-       addElement(inputValue);
+       update();
   }
     };
-    console.log(inputValue)
-    console.log(elements)
+
 
 
 
@@ -35,12 +71,12 @@ const onKeyPress = (event) => {
 
 return (
     <>
-<Header setInputValue={setInputValue} inputValue={inputValue} onKeyPress={onKeyPress} addElement={addElement}/>
+<Header setInputValue={setInputValue} inputValue={inputValue} onKeyPress={onKeyPress} addElement={update}/>
 <div className="container">
       <ul>
-          {elements.map((todo) => {
-              return <li className="d-flex justify-content-between align-items-center" key={todo.id}>{todo.todo}<span className="me-3">
-                  <i className="fa-solid fa-xmark" onClick={()=> handleDeleteElement(todo.id)}></i></span></li>
+          {elements.map((todo, index) => {
+              return <li className="d-flex justify-content-between align-items-center" key={index}>{todo.label}<span className="me-3">
+                  <i className="fa-solid fa-xmark" onClick={()=> handleDeleteElement(todo.label)}></i></span></li>
           })}
       </ul>
       </div>
